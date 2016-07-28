fd60876a9e3395cff401a730a3e1b56f5f20ee27
Aashish Pandey <aashishkpandey@gmail.com>
2016-07-28 17:00:16 +0530

    Added dependency
    df

---
+++ dependency-basic-text-analysis-shinyapp.R
@@ -0,0 +1,14 @@
+try(require("shiny")||install.packages("shiny"))
+try(require("tm")||install.packages("tm"))
+try(require("wordcloud")||install.packages("wordcloud"))
+
+try(require("qdap")||install.packages("qdap"))
+try(require("RWeka")||install.packages("RWeka"))
+try(require("igraph")||install.packages("igraph"))
+
+library("shiny")
+library("tm")
+library("wordcloud")
+library("qdap")
+library("RWeka")
+library("igraph")
