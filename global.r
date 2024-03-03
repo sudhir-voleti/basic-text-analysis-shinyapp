105e53877b19150f20d0ac3fd1383be6b652218f
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-03-03 17:46:34 +0530

    Update global.r

--- global.r
+++ global.r
@@ -4,29 +4,19 @@ library(visNetwork)
 
 
 puncts = c(",", "\\.", "!", "\\?");
-
 text.clean = function(x,html_tags,numbers)                    # text data
 { #require("tm")
   x  =  iconv(x, "latin1", "ASCII", sub="") # Keep only ASCII characters
   x  =  tolower(x)
-  if(html_tags){
-    x  =  gsub("<.*?>", " ", x)             # regex for removing HTML tags
-  }
-  if(numbers){
-    x  =  removeNumbers(x) 
-  }# convert to lower case characters
-  
+  if(html_tags){ x  =  gsub("<.*?>", " ", x) }# regex for removing HTML tags
+  if(numbers){ x  =  removeNumbers(x) } # convert to lower case characters
   x  =  gsub("[^[:alnum:]]", " ", x)        # keep only alpha numeric
-   
-  
-                     # removing numbers
   x  =  stripWhitespace(x)                  # removing white space
   x  =  gsub("^\\s+|\\s+$", "", x)          # remove leading and trailing white space
   return(x)
 }
 
 dtm.word.count <- function(dtm) {
-  
   if (ncol(dtm) > 1000) {
     tst = round(ncol(dtm)/100)  # divide DTM's cols into 100 manageble parts
     a = rep(tst,99)
@@ -38,20 +28,16 @@ dtm.word.count <- function(dtm) {
     for (i in 1:(length(b)-1)) {
       tempdtm = dtm[,(b[i]+1):(b[i+1])]
       s = colSums(as.matrix(tempdtm))
-      ss.col = c(ss.col,s)
-    }
+      ss.col = c(ss.col,s) }
   } else {
     ss.col = colSums(as.matrix(dtm))
   }
-  
   tsum = ss.col
   # tsum = tsum[order(tsum, decreasing = T)]       #terms in decreasing order of freq
   return(tsum)
 }
 
-
 dtm.word.cloud <- function(count = count, max.words = 100,title = "Title"){
-  
   if (class(count)[1] == "DocumentTermMatrix"|class(count)[1] == "simple_triplet_matrix")
   {
     tsum = dtm.word.count(count)
@@ -60,7 +46,6 @@ dtm.word.cloud <- function(count = count, max.words = 100,title = "Title"){
   }
   
   if (class(tsum) != "numeric") stop("Give input as wordcount or DocumentTermMatrix")
-  
   wordcloud(names(tsum), tsum,     # words, their freqs 
             scale = c(4, 1),     # range of word sizes
             min.freq = .01,                     # min.freq of words to consider
@@ -75,18 +60,12 @@ distill.cog.tcm = function(mat1, # input TCM or DTM MAT
                            s,    # no. of central nodes
                            k1){  # max no. of connections  
   require(igraph)
-  
   mat1 = as.matrix(mat1)
-  
-  if (mattype == "DTM"){
-    mat1 = tcrossprod(t(mat1))
-  }
+  if (mattype == "DTM"){ mat1 = tcrossprod(t(mat1)) }
   
   if (mattype == "TCM"){
     mat1 = as.matrix(mat1)
-    mat1 = mat1 + t(mat1)
-  }
-  
+    mat1 = mat1 + t(mat1) }
   
   if (ncol(mat1) > 1000) {
     tst = round(ncol(mat1)/100)  # divide mat1's cols into 100 manageble parts
@@ -105,18 +84,14 @@ distill.cog.tcm = function(mat1, # input TCM or DTM MAT
     ss.col = colSums(as.matrix(mat1))
   }
   
-  # a = colSums(mat1) # collect colsums into a vector obj a
   a = ss.col
   b = order(-a)     # nice syntax for ordering vector in decr order  
-  
   mat2 = mat1[b, b]     # order both rows and columns along vector b
-  
   diag(mat2) =  0
+
   
   ## +++ go row by row and find top k adjacencies +++ ##
-  
   wc = NULL
-  
   for (i1 in 1:s){ 
     thresh1 = mat2[i1,][order(-mat2[i1, ])[k1]]
     mat2[i1, mat2[i1,] < thresh1] = 0   # neat. didn't need 2 use () in the subset here.
@@ -179,11 +154,6 @@ dtm.tcm.creator <- function(text,id = "",
   tok_fun = word_tokenizer  # using word & not space tokenizers
   
   if (bigram.encoding == TRUE){
-    
-    # data = data.frame(id = 1:length(text),text = text, stringsAsFactors = F)
-    
-    # print("finding bi-grams for encoding with selected criteria")
-    
     it_0 = itoken( text,
                    tokenizer = tok_fun,
                    ids = id,
@@ -191,70 +161,41 @@ dtm.tcm.creator <- function(text,id = "",
     
     vocab = create_vocabulary(it_0, ngram = c(2L, 2L))
     pruned_vocab = data.frame(prune_vocabulary(vocab, term_count_min = bigram.min.freq))
-    
     replace_list = pruned_vocab$term[order(pruned_vocab$term_count, decreasing = T)]
     
     # Cut the bi-grams upto 200 words
-    
     if (length(replace_list) > 200){
-      replace_list = replace_list[1:200]
-    }
+      replace_list = replace_list[1:200] }
     
     if (length(replace_list) > 1){     # 0, my edit 1
       text = paste("",text,"")
-      
       pb <- txtProgressBar(min = 1, max = (length(replace_list)), style = 3) ; i = 0
-      
-      # print(paste("Encoding",length(replace_list),"bi-grams as unigram"))
       for (term in replace_list){
         i = i + 1
         focal.term = gsub("_", " ",term)        # in case dot was word-separator
         replacement.term = term
         text = gsub(paste("",focal.term,""),paste("",replacement.term,""), text)
-        # setTxtProgressBar(pb, i)
       }                  
     } else {
       print("No bigram to encode with selected criteria")}
   }
   
-  # print("Creating Document Term Matrix")
   # Create DTM
   it_m = itoken(text,
                 tokenizer = tok_fun,
                 ids = id,
                 progressbar = F)
-  
   vocab = create_vocabulary(it_m)
   pruned_vocab = prune_vocabulary(vocab,
                                   term_count_min = min.dtm.freq)
-  
   vectorizer = vocab_vectorizer(pruned_vocab)
-  
   dtm_m  = create_dtm(it_m, vectorizer)
-  
-  # print("Creating Term Co-occurrence Matrix")
-  
-  # vectorizer = vocab_vectorizer(pruned_vocab,
-  #                               grow_dtm = FALSE,
-  #                               skip_grams_window = skip.grams.window)
-  # 
-  # tcm = create_tcm(it_m, vectorizer) # func to build a TCM
-  
-  # print("Done!!")
-  
   out = list(dtm = dtm_m)#, tcm = tcm)
   return(out)
 }
 
 bigram.collocation <- function(text1){   # text1 from readLines() is input
-  
-  # require(magrittr)
-  # require(tidytext)
-  # require(dplyr)
-  # require(tidyr)
-  
   text1 = gsub('<.*?>', "", text1)   # drop html junk
-  
   # create words df
     unnest_tokens(word, text1) |>
     anti_join(stop_words) |> 
@@ -278,13 +219,10 @@ bigram.collocation <- function(text1){   # text1 from readLines() is input
   new_df = bigram_df |> mutate(k1 = 0) |> mutate(k2 = 0) # |>
   
   for (i1 in 1:nrow(bigram_df)){
-    
     a0 = which(bigram_df$word1[i1] == text_df$word) 
     ifelse((length(a0) > 0), { new_df$k1[i1] = text_df$n[a0] }, next ) 
-    
     a1 = which(bigram_df$word2[i1] == text_df$word) 
     ifelse((length(a1) > 0), { new_df$k2[i1] = text_df$n[a1] }, next ) 
-    
   } # i1 loop ends
   
   new_df1 = new_df |> filter(n > 1) |> mutate(coll.ratio = (n*nrow(new_df))/(k1*k2)) |>
