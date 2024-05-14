4384e15b5790df68d8544e717aab65b0a76d6686
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-05-14 09:32:16 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,6 +1,29 @@
 #################################################
 #               Basic Text Analysis             #
 #################################################
+library(shiny)
+library(text2vec)
+library(tm)
+library(tokenizers)
+library(wordcloud)
+library(slam)
+library(visNetwork)
+
+library(stringi)
+library(magrittr)
+library(tidytext)
+library(dplyr)
+library(tidyr)
+library(igraph)
+library(tools)
+library(pdftools)
+
+if (!require(shinyWidgets)) {install.packages("shinyWidgets")};  library(shinyWidgets)
+if (!require(stringr)) {install.packages("stringr")};  library(stringr)
+if (!require(DT)){install.packages('DT')}; library('DT')
+if (!require(quanteda)){install.packages('quanteda')}; library('quanteda')
+
+options(shiny.maxRequestSize = 50*1024^2) 
 
 shinyServer(function(input, output,session) {
   set.seed=2092014   
