4d446eea34965874ba888c463ae811437af5eef2
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-06-03 18:11:52 +0530

    Update global.r

--- global.r
+++ global.r
@@ -54,68 +54,69 @@ dtm.word.cloud <- function(count = count, max.words = 100,title = "Title"){
   title(sub = title)     # title for the wordcloud display
 }   
 
-distill.cog.tcm = function(mat1, # input TCM or DTM MAT
-                           mattype = "DTM", # "DTM" or TCM
-                           title, # title for the graph
-                           s,    # no. of central nodes
-                           k1){  # max no. of connections  
-  require(igraph)
-  mat1 = as.matrix(mat1)
-  if (mattype == "DTM"){ mat1 = tcrossprod(t(mat1)) }
+#distill.cog.tcm = function(mat1, # input TCM or DTM MAT
+#                           mattype = "DTM", # "DTM" or TCM
+#                           title, # title for the graph
+#                           s,    # no. of central nodes
+#                           k1){  # max no. of connections  
+#  require(igraph)
+#  mat1 = as.matrix(mat1)
+#  if (mattype == "DTM"){ mat1 = tcrossprod(t(mat1)) }
   
-  if (mattype == "TCM"){
-    mat1 = as.matrix(mat1)
-    mat1 = mat1 + t(mat1) }
+#  if (mattype == "TCM"){
+#    mat1 = as.matrix(mat1)
+#    mat1 = mat1 + t(mat1) }
   
-  if (ncol(mat1) > 1000) {
-    tst = round(ncol(mat1)/100)  # divide mat1's cols into 100 manageble parts
-    a = rep(tst,99)
-    b = cumsum(a);rm(a)
-    b = b[-which(b >= ncol(mat1))]
-    b = c(0,b,ncol(mat1))
+#  if (ncol(mat1) > 1000) {
+#    tst = round(ncol(mat1)/100)  # divide mat1's cols into 100 manageble parts
+#    a = rep(tst,99)
+#    b = cumsum(a);rm(a)
+#    b = b[-which(b >= ncol(mat1))]
+#    b = c(0,b,ncol(mat1))
     
-    ss.col = c(NULL)
-    for (i in 1:(length(b)-1)) {
-      tempmat1 = mat1[,(b[i]+1):(b[i+1])]
-      su = colSums(as.matrix(tempmat1))
-      ss.col = c(ss.col,su);rm(su)
-    }
-  } else {
-    ss.col = colSums(as.matrix(mat1))
-  }
+#    ss.col = c(NULL)
+#    for (i in 1:(length(b)-1)) {
+#      tempmat1 = mat1[,(b[i]+1):(b[i+1])]
+#      su = colSums(as.matrix(tempmat1))
+#      ss.col = c(ss.col,su);rm(su)
+#    }
+#  } else {
+#    ss.col = colSums(as.matrix(mat1))
+#  }
   
-  a = ss.col
-  b = order(-a)     # nice syntax for ordering vector in decr order  
-  mat2 = mat1[b, b]     # order both rows and columns along vector b
-  diag(mat2) =  0
+#  a = ss.col
+#  b = order(-a)     # nice syntax for ordering vector in decr order  
+#  mat2 = mat1[b, b]     # order both rows and columns along vector b
+#  diag(mat2) =  0
 
   
   ## +++ go row by row and find top k adjacencies +++ ##
-  wc = NULL
-  for (i1 in 1:s){ 
-    thresh1 = mat2[i1,][order(-mat2[i1, ])[k1]]
-    mat2[i1, mat2[i1,] < thresh1] = 0   # neat. didn't need 2 use () in the subset here.
-    #mat2[i1, mat2[i1,] > 0 ] = 1
-    word = names(mat2[i1, mat2[i1,] > 0])
-    mat2[(i1+1):nrow(mat2), match(word,colnames(mat2))] = 0
-    wc = c(wc,word)
-  } 
+#  wc = NULL
+#  for (i1 in 1:s){ 
+#    thresh1 = mat2[i1,][order(-mat2[i1, ])[k1]]
+#    mat2[i1, mat2[i1,] < thresh1] = 0   # neat. didn't need 2 use () in the subset here.
+#    #mat2[i1, mat2[i1,] > 0 ] = 1
+#    word = names(mat2[i1, mat2[i1,] > 0])
+#    mat2[(i1+1):nrow(mat2), match(word,colnames(mat2))] = 0
+#    wc = c(wc,word)
+#  } 
   
-  mat3 = mat2[match(wc, colnames(mat2)), match(wc, colnames(mat2))]
-  ord = colnames(mat2)[which(!is.na(match(colnames(mat2), colnames(mat3))))]  # removed any NAs from the list
-  mat4 = mat3[match(ord, colnames(mat3)), match(ord, colnames(mat3))]
+#  mat3 = mat2[match(wc, colnames(mat2)), match(wc, colnames(mat2))]
+#  ord = colnames(mat2)[which(!is.na(match(colnames(mat2), colnames(mat3))))]  # removed any NAs from the list
+#  mat4 = mat3[match(ord, colnames(mat3)), match(ord, colnames(mat3))]
   
-  graph <- graph.adjacency(mat4, mode = "undirected", weighted=T)    # Create Network object
+#  graph <- graph.adjacency(mat4, mode = "undirected", weighted=T)    # Create Network object
   
+#  graph = simplify(graph) 
+#  #V(graph)$color[1:s] = "green"
+#  #V(graph)$color[(s+1):length(V(graph))] = "pink"
   
-  graph = simplify(graph) 
-  #V(graph)$color[1:s] = "green"
-  #V(graph)$color[(s+1):length(V(graph))] = "pink"
-  
-  graph = delete.vertices(graph, V(graph)[ degree(graph) == 0 ]) # delete singletons?
-  E(graph)$width <- E(graph)$weight/min(E(graph)$weight)  
-  visIgraph(graph,layout = 'layout.kamada.kawai')
-} 
+#  graph = delete.vertices(graph, V(graph)[ degree(graph) == 0 ]) # delete singletons?
+#  E(graph)$width <- E(graph)$weight/min(E(graph)$weight)  
+#  visIgraph(graph,layout = 'layout.kamada.kawai')
+#} # func ends 
+
+
 dtm.tcm.creator <- function(text,id = "",
                             std.clean = TRUE,
                             std.stop.words = TRUE,
