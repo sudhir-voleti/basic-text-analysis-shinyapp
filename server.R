449b4488007f1d218471658135e4312559882887
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-06-03 20:06:33 +0530

    Update server.R

--- server.R
+++ server.R
@@ -276,6 +276,18 @@ dtm_idf =  eventReactive(input$apply,{
 #    }
 #  })
 
+output$prompt1 <- renderUI({ 
+  
+   HTML("Context: This image shows the <b>150 token<b> wordcloud from <b>400+ reviews on itunes of Uber services in the US.</b><br><br>
+
+  Task: Analyze the wordcloud. Based on frequently occurring tokens, speculate about what <b>people are saying the reviews?</b><br><br>
+
+  Format: Make reasonable assumptions. Make a table showing the top few themes that seem to emerge. Table columns should be:<br><br>
+
+  [1] Main Token or group of tokens,<br> 
+  [2] Possible theme, <br>
+  [3] Reasoning" }),
+  
   output$idf_size  <- renderPrint({
     if (is.null(input$file)|input$apply==0) {return(NULL)}
     else {
